c4d308025423708a64febcfd84e8df02bf1f6c6c
Sparsh Malhotra <sparshmalhotraaa@gmail.com>
2025-03-21 15:37:35 +0530

    Added show incidents toggle

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
+import IncidentToggle from "@/components/IncidentToggle";
 import Map from "@/components/Map";
+import { useState } from "react";
 
 const Home = () => {
+  const [showIssues, setShowIssues] = useState(false);
+
+  const toggleIssuesVisibility = () => {
+    setShowIssues(prev => !prev);
+  };
+  
   return (
     <div className="relative h-screen w-full overflow-hidden">
-      <Map className="h-full w-full" />
+      <Map className="h-full w-full" showIssues={showIssues} />
+      <IncidentToggle
+        showIssues={showIssues}
+        onToggle={toggleIssuesVisibility}
+        className="absolute top-20 left-4 z-20"
+      />
     </div>
   );
 };
